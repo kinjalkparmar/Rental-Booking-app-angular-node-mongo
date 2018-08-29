d8a597aad5dd46c915d221257ffb4da0c9ea4c97
kinjalk parmar <parmarkinjalk@gmail.com>
2018-08-28 17:07:12 -0700

    Added Endpoints for creation of rental and Search Rental by city implemented

--- server/routes/rentals.js
+++ server/routes/rentals.js
@@ -1,21 +1,14 @@
 const express = require('express');
 const router = express.Router();
 const Rental = require('../models/rental');
+const MongooseHelpers = require('../helpers/mongoose');
+const User = require('../models/user');
+
 
 const userctrl = require('../controllers/user')
 
 router.get('/secret', userctrl.authMiddleware, function(req, res){
     res.json({"secret": true})
-})
-
-
-
-router.get('', function(req, res){
-   Rental.find({})
-    .select('-bookings')
-    .exec(function(err, foundRentals){
-        res.json(foundRentals)
-    })
 })
 
 router.get('/:id', function(req, res){
@@ -33,5 +26,50 @@ router.get('/:id', function(req, res){
 
     
  })
+
+
+
+router.get('', function(req, res){
+    const city = req.query.city;
+    const query = city ? {city: city.toLowerCase()} : {};
+    
+    Rental.find(query)
+            .select('-bookings')
+            .exec(function(err, foundRentals){
+        if(err){
+            return res.status(422).send({errors: MongooseHelpers.normalizeErrors(err.errors)})
+        }
+        if(city && foundRentals.length === 0){
+            return res.status(422).send({errors: [{title: 'No Rentals found', detail: `There are no rentals for city ${city}`}]})
+        }
+        
+        return res.json(foundRentals)
+    })
+    
+
+   
+})
+
+router.post('', userctrl.authMiddleware, function(req, res){
+    const {title, city, street, category, image, shared, bedrooms, description, dailyRates} = req.body;
+    const user = res.locals.user;
+    const rental = new Rental({title, city, street, category, image, shared, bedrooms, description, dailyRates});
+    rental.user = user;
+
+    Rental.create(rental, function(err, newRental){
+        if(err){
+            return res.status(422).send({errors: MongooseHelpers.normalizeErrors(err.errors)})
+        }
+
+        User.update({_id: user.id}, {$push: {rentals: newRental}}, function(){})
+        return res.json(newRental);
+    })
+
+
+
+
+})
+
+
 
 module.exports = router;
